d5f64aca5c656f6b35eca8c76c7a0c16beb86dd4
Oleg Chebulaev <alegch@undev.ru>
2013-06-26 12:53:09 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Progul::Application.config.secret_key_base = '081107118baf544d66cca07038b7663b76a70bd1a5f57701c4da1f3ced54be6f059d2c16524f13baaf0a6ac016f44bd3eab0675e5308ad07852b5365781bbfbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Progul::Application.config.session_store :cookie_store, key: '_progul_session'
