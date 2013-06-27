8c50d17896ccd879d25868d62b15f59a8a40488d
Oleg Chebulaev <alegch@undev.ru>
2013-06-27 10:51:21 +0400

    add web add api hierarhi fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Progul::Application.routes.draw do
-  get "welcome/index"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  scope module: :web do
+    root 'welcome#index'
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- test/controllers/web/welcome_controller_test.rb
+++ test/controllers/web/welcome_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionController::TestCase
+class Web::WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
