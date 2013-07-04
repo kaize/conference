b8cf96f8fb4b4dc5b5c4ad821d634ec937e85288
Oleg Chebulaev <alegch@undev.ru>
2013-07-04 15:26:26 +0400

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 admin = User.find_or_initialize_by(email: ENV['admin_email'])
 admin.password = ENV['admin_password']
 admin.password_confirmation = ENV['admin_password']
+admin.name = "Admin user"
 admin.admin = true
 admin.save!
 
 user = User.find_or_initialize_by(email: ENV['user_email'])
 user.password = ENV['user_password']
 user.password_confirmation = ENV['user_password']
+user.name = "User"
 user.save!
