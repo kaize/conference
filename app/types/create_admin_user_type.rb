53cb98c8c5e062cac7d3e832da79468069a26f81
Oleg Chebulaev <alegch@undev.ru>
2013-07-07 22:51:00 +0400

    fix validations

--- app/types/create_admin_user_type.rb
+++ app/types/create_admin_user_type.rb
@@ -2,6 +2,13 @@ class CreateAdminUserType < User
   include BaseType
 
   permit :email, :password, :password_confirmation, :first_name, :last_name, :state_event, :city, :company, :position, :about, :role, :show_as_participant, :admin
-  validates :password, :password_confirmation, :first_name, :last_name, :city, :company, :position, :about, presence: true
+
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :city, presence: true
+  validates :company, presence: true
+  validates :position, presence: true
+  validates :about, presence: true
 end
 
