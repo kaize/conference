d368ccece1045a8baa787c82c84fb0444f0ed859
Oleg Chebulaev <alegch@undev.ru>
2013-07-09 15:33:30 +0400

    remove name column from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130707185732) do
+ActiveRecord::Schema.define(version: 20130709111936) do
 
   create_table "halls", force: true do |t|
     t.string   "name"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20130707185732) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.string   "city"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 admin = User.find_or_initialize_by(email: ENV['admin_email'])
 admin.password = ENV['admin_password']
 admin.password_confirmation = ENV['admin_password']
-admin.name = "Admin user"
 admin.first_name = "Админ"
 admin.last_name = "Админов"
 admin.company = "Компания"
@@ -14,9 +13,8 @@ admin.save!
 user = User.find_or_initialize_by(email: ENV['user_email'])
 user.password = ENV['user_password']
 user.password_confirmation = ENV['user_password']
-user.name = "User"
-user.first_name = "Админ"
-user.last_name = "Админов"
+user.first_name = "Юзер"
+user.last_name = "Юзеров"
 user.company = "Компания"
 user.position = "Позиция"
 user.city = "Ульяновск"
